79708b58f422cc3d16befaebd62b32f6b31dc381
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-16 16:10:43 -0300

    Class02 - Atv2

--- alura-studies/src/App.tsx
+++ alura-studies/src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React from "react";
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi!</h1>
+      <Button />
     </div>
   );
 }
