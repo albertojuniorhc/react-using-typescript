0c4552caa36be06bd207139a7a11f2b718d29af0
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-17 20:33:44 -0300

    Class05 - Atv6

--- alura-studies/src/components/Form/index.tsx
+++ alura-studies/src/components/Form/index.tsx
@@ -3,9 +3,19 @@ import Button from "../Button";
 import style from "./Form.module.scss";
 
 class Form extends React.Component {
+  state = {
+    task: "",
+    time: "00:00:00"
+  }
+
+  addTask(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault();
+    console.log('state: ', this.state )
+  }
+
   render() {
     return (
-      <form className={style.newTask}>
+      <form className={style.newTask} onSubmit={this.addTask.bind(this)}>
         <div className={style.inputContainer}>
           <label htmlFor="task">Add a new study</label>
           <input
@@ -13,6 +23,8 @@ class Form extends React.Component {
             name="task"
             id="task"
             placeholder="What do you wanna study?"
+            value={this.state.task}
+            onChange={event => this.setState({ ...this.setState, task: event.target.value })}
             required
           />
         </div>
@@ -25,6 +37,8 @@ class Form extends React.Component {
             id="time"
             min="00:00:00"
             max="01:30:00"
+            value={this.state.time}
+            onChange={event => this.setState({ ...this.setState, time: event.target.value })}
             required
           />
         </div>
