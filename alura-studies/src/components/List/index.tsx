ff03d3fd967267119051c40a3615e8a2741c0546
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-17 20:11:33 -0300

    Class05 - Atv2

--- alura-studies/src/components/List/index.tsx
+++ alura-studies/src/components/List/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import style from "./List.module.scss";
 import Item from "./Item";
+import { useState } from "react";
 
 function List() {
-  const tasks = [
+  const [tasks, setTasks] = useState([
     {
       task: "React",
       time: "02:00:00",
@@ -16,11 +17,17 @@ function List() {
       task: "TypeScript",
       time: "02:00:00",
     },
-  ];
+  ]);
 
   return (
     <aside className={style.taskList}>
-      <h2>Today studies</h2>
+      <h2
+        onClick={() => {
+          setTasks([...tasks, { task: "UseState", time: "01:00:00" }]);
+        }}
+      >
+        Today studies
+      </h2>
       <ul>
         {tasks.map((item, index) => (
           <Item key={index} {...item} />
