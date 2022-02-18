af53d889f6827d7770a0446cdc4a8ff393e4ff4b
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-18 16:30:14 -0300

    Class06 - Atv8

--- alura-studies/src/components/Stopwatch/index.tsx
+++ alura-studies/src/components/Stopwatch/index.tsx
@@ -1,16 +1,25 @@
-import { timeToSeconds } from "../../common/utils/date";
+import { useState } from "react";
+import { timeToSeconds } from "../../common/utils/time";
+import { ITask } from "../../types/task";
 import Button from "../Button";
 import Clock from "./Clock";
-import style from "./Stopwatch.module.scss"
+import style from "./Stopwatch.module.scss";
 
-export default function Stopwatch() {
-  console.log('conversion: ', timeToSeconds('01:01:01') )
+interface Props {
+  selected: ITask | undefined;
+}
+
+export default function Stopwatch({ selected }: Props) {
+  const [time, setTime] = useState<number>();
+  if (selected?.time) {
+    setTime(timeToSeconds(selected.time));
+  }
   return (
     <div className={style.stopwatch}>
       <p className={style.title}>Select a card and start the Stopwatch</p>
-
+      Time: {time}
       <div className={style.clockWrapper}>
-          <Clock />
+        <Clock />
       </div>
       <Button>Start</Button>
     </div>
