929b47366a51d4f5c21b7595bd7baa3bca2eb43e
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-18 15:27:02 -0300

    Class06 - Atv2

--- alura-studies/src/components/List/Item/index.tsx
+++ alura-studies/src/components/List/Item/index.tsx
@@ -1,10 +1,32 @@
 import { ITask } from "../../../types/task";
 import style from "../List.module.scss";
 
-export default function Item({ task, time, selected, finished, id }: ITask) {
-  console.log('Item atual: ', { task, time, selected, finished, id })
+interface Props extends ITask {
+  selectTask: (selectedTask: ITask) => void;
+}
+
+export default function Item({
+  task,
+  time,
+  selected,
+  finished,
+  id,
+  selectTask,
+}: Props) {
+  console.log("Item atual: ", { task, time, selected, finished, id });
   return (
-    <li className={style.item}>
+    <li
+      className={style.item}
+      onClick={() =>
+        selectTask({
+          task,
+          time,
+          selected,
+          finished,
+          id,
+        })
+      }
+    >
       <h3>{task}</h3>
       <span>{time}</span>
     </li>
--- alura-studies/src/pages/App.tsx
+++ alura-studies/src/pages/App.tsx
@@ -7,10 +7,16 @@ import style from "./App.module.scss";
 
 function App() {
   const [tasks, setTasks] = useState<ITask[]>([]);
+  const [selected, setSelected] = useState<ITask>();
+
+  function selectTask(selectedTask: ITask) {
+    selectTask(selectedTask);
+  }
+
   return (
     <div className={style.AppStyle}>
       <Form setTasks={setTasks} />
-      <List tasks={tasks} />
+      <List tasks={tasks} selectTask={selectTask}/>
       <Stopwatch />
     </div>
   );
