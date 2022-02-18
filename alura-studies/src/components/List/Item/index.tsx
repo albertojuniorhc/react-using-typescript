732d57fbc2040ec5f9de8aab2aa56e443d2d9ec8
Alberto Junior <albertojuniorhc@gmail.com>
2022-02-17 21:48:48 -0300

    Class05 - Atv8

--- alura-studies/src/components/List/Item/index.tsx
+++ alura-studies/src/components/List/Item/index.tsx
@@ -1,6 +1,8 @@
+import { ITask } from "../../../types/task";
 import style from "../List.module.scss";
 
-export default function Item({ task, time }: { task: string; time: string }) {
+export default function Item({ task, time, selected, finished, id }: ITask) {
+  console.log('Item atual: ', { task, time, selected, finished, id })
   return (
     <li className={style.item}>
       <h3>{task}</h3>
